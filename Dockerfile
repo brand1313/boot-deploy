8f9e07a8496ffdf9f19ba3b65cacceff7d5f0740
Leo Kim <leo.kim@osckorea.com>
2021-10-15 11:05:23 +0900

    first commit

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM openjdk:11-jre-slim
+
+WORKDIR /home/ec2-user/
+
+COPY demo-0.0.1-SNAPSHOT.jar .
+
+CMD java -jar demo-0.0.1-SNAPSHOT.jar
+
---
+++ src/main/java/com/test/kube/kubetest/KubetestApplication.java
@@ -0,0 +1,13 @@
+package com.test.kube.kubetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubetestApplication.class, args);
+	}
+
+}
